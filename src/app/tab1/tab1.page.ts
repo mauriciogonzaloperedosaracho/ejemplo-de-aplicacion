173567ea68facd8347067a2dcc088c27939a0953
unknown <gonchiperedo77@gmail.com>
2020-08-26 18:09:12 -0400

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  dado1 = 0;
+  dado2 = 0;
+  suma = 0;
+  eleccion = '';
+  resultado: string;
+
   constructor() {}
+
+  calcularSuma() {
+
+    this.dado1 = Math.floor(Math.random() * 6) + 1;
+    this.dado2 = Math.floor(Math.random() * 6) + 1;
+
+    this.suma = this.dado1 + this.dado2;
+    if (this.suma >= 2 && this.suma <= 6) {
+      this.resultado = 'menor';
+    }
+    else if (this.suma > 7) {
+      this.resultado = 'mayor';
+    }
+    else if (this.suma === 7) {
+      this.resultado = 'casa';
+    }
+    // En php: $this->suma = $this->dado1 + $this->dado2
+  }
+
+  elegirOpcion(opcion: string) {
+    this.eleccion = opcion;
+  }
 
 }
